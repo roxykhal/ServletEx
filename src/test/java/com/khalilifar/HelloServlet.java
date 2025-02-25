a8a8df000458c0269d7fae9abb888f479d78fea5
Roxy Khalilifar <roxykhal@hotmail.com>
2025-02-25 09:46:14 +0000

    get a response

--- src/test/java/com/khalilifar/HelloServlet.java
+++ src/test/java/com/khalilifar/HelloServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 
 public class HelloServlet extends HttpServlet {
@@ -12,8 +13,11 @@ public class HelloServlet extends HttpServlet {
     //gets executed everytime you run the method
     //Data coming from client is stored in req
     //Data you want to send back to client is stored in res
-    public void service(HttpServletRequest req, HttpServletResponse res) {
+    public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
         System.out.println("In service");
+
+        //get a response
+        res.getWriter().println("Hello world");
 
 
     }
